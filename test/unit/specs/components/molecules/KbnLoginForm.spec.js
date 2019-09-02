d59da13633df9b0ed19b50a7df36308d652ad5dc
Kiyotaka <t10kiyotaka@gmail.com>
2019-09-02 23:38:47 +0900

    Add test

--- test/unit/specs/components/molecules/KbnLoginForm.spec.js
+++ test/unit/specs/components/molecules/KbnLoginForm.spec.js
@@ -11,5 +11,57 @@ describe('KbnLoginForm', () => {
       loginForm.vm.$nextTick(done)
     })
 
+    describe('email', () => {
+      describe('required', () => {
+        describe('Nothing is entered', () => {
+          it('validation.email.required is invalid', () => {
+            loginForm.setData({ email: ''})
+            expect(loginForm.vm.validation.email.required).to.equal(false)
+          })
+        })
+
+        describe('Something is entered', () => {
+          it('validation.email.required is valid', () => {
+            loginForm.setData({ email: 'foo@domain.com'})
+            expect(loginForm.vm.validation.email.required).to.equal(true)
+          })
+        })
+      })
+
+      describe('fromat', () => {
+        describe('invalid email address format', () => {
+          it('validation.email.format is invalid', () => {
+            loginForm.setData({ email: 'foobar' })
+            expect(loginForm.vm.validation.email.format).to.equal(false)
+          })
+        })
+
+        describe('valid email address format', () => {
+          it('validation.email.format is valid', () => {
+            loginForm.setData({ email: 'foo@domain.com' })
+            expect(loginForm.vm.validation.email.format).to.equal(true)
+          })
+        })
+      })
+    })
+
+    descrive('password', () => {
+      describe('required', () => {
+        describe('Nothing is entered', () => {
+          it('validation.password.required is invalid', () => {
+            loginForm.setData({ password: '' })
+            expect(loginForm.vm.validation.password.required).to.equal(false)
+          })
+        })
+
+        describe('Something is entered', () => {
+          it('validation.password.required is valid', () => {
+            loginForm.setData({ password: 'xxxx' })
+            expect(loginForm.vm.validation.password.required).to.equal(true)
+          })
+        })
+      })
+    })
   })
+  
 })
