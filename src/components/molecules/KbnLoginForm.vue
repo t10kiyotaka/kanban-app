11e1f75b38971837e0db397a60031c9d328584bd
Kiyotaka <t10kiyotaka@gmail.com>
2019-09-04 23:18:25 +0900

    Impl KbnLoginForm component

--- src/components/molecules/KbnLoginForm.vue
+++ src/components/molecules/KbnLoginForm.vue
@@ -49,3 +49,88 @@
     </div>
   </form>
 </template>
+
+<script>
+import KbnButton from '@/components/atoms/KbnButton'
+
+const REGEX_EMAIL = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/
+const required = val => !!val.trim()
+
+export default {
+  name: 'KbnLoginForm',
+
+  components: {
+    KbnButton
+  },
+
+  props: {
+    onlogin: {
+      type: Function,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      email: '',
+      password: '',
+      progress: false,
+      error: ''
+    }
+  },
+
+  computed: {
+    validation() {
+      return {
+        email: {
+          required: required(this.email),
+          format: REGEX_EMAIL.test(this.email)
+        },
+        password: {
+          required: required(this.password)
+        }
+      }
+    },
+
+    valid() {
+      const validation = this.validation
+      const fields = Object.keys(validation)
+      let valid = true
+      for(let i = 0; i < fields.length; i++) {
+        const field = fields[i]
+        valid = Object.keys(validation[field])
+          .every(key => validation[field][key])
+          if(!valid) { break }
+      }
+      return valid
+    },
+
+    disableLoginAction() {
+      return !this.valid || this.progress
+    }
+  },
+
+  methods: {
+    resetError() {
+      this.error = ''
+    },
+
+    handleClick(ev) {
+      if(this.disableLoginAction) { return } // Guard for preventing process if there is a defect.
+
+      this.progress = true // This means processing login
+      this.error = ''
+
+      this.$nextTick(() => {
+        this.onlogin({ email: this.email, password: this.password })
+          .catch(err => {
+            this.error = err.message
+          })
+          .then(() => {
+            this.progress =false
+          })
+      })
+    }
+  }
+}
+</script>
