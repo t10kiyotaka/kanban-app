ad3e2255a1e1cb449153053c5038f4880306c809
Kiyotaka <t10kiyotaka@gmail.com>
2019-09-07 10:36:23 +0900

    Make a mock of login API endpoint

--- build/dev-server.js
+++ build/dev-server.js
@@ -6,5 +6,26 @@ module.exports = app => {
   // Install a midle ware to parse HTTP request body as JSON.
   app.use(bodyParser.json())
 
-  // TODO: Add API impl here.
+  const users = {
+    'foo@domain.com': {
+      password: '12345678',
+      userId: 1,
+      token: '1234567890abcdef'
+    }
+  }
+
+  // endpoint of login API '/auth/login'
+  app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body
+    const user = users[email]
+    if(user) {
+      if(user.password !== password) {
+        res.status(401).json({ message: 'Failed to login.' })
+      } else {
+        res.json({ userId: user.userId, token: user.token })
+      }
+    } else {
+      res.status(404).json({ message: 'The user is not registered.' })
+    }
+  })
 }
